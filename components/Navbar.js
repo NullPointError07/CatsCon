73e8c8b70edeeffa3ecfb89169755b87da815b55
MD Taukir Alam <taukir007alam@gmail.com>
2023-10-07 18:28:54 +0600

    initialized project

---
+++ components/Navbar.js
@@ -0,0 +1,66 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { FaBars } from "react-icons/fa";
+import NavbarResponsive from "./NavbarResponsive";
+
+const Navbar = () => {
+  // dropdown state for Find Cats
+  const [dropdown, setDropDown] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  // toggle function for Find Cats
+  const toggleDropDown = () => {
+    setDropDown(!dropdown);
+  };
+
+  // toggle function for navbar
+  const toggleNavbar = () => {
+    setNavbar(!navbar);
+  };
+
+  const navItems = ["Find Cats", "Benefits", "FAQ", "About Us"];
+
+  return (
+    <div className="flex justify-between items-center bg-[#FFFFFF] sticky top-0 z-30 2xl:px-[140px] xl:px-[105px] lg:px-[93.33px] md:px-[30px] px-[22px] cursor-pointer">
+      <div className="flex items-center space-x-4 md:space-x-3 lg:space-x-8">
+        <div>
+          <button onClick={toggleNavbar} className="lg:hidden theme-1">
+            <FaBars />
+          </button>
+        </div>
+        <div className="flex items-center ">
+          <Image src="/CatsIcon.png" alt="CatsIcon" width={75} height={25} />
+          <h1 className="theme-1 text-2xl">
+            Cats<span className="theme-2">Ezy</span>
+          </h1>
+        </div>
+        <div className="hidden lg:flex space-x-2 md:space-x-4 lg:space-x-8 text-xl ">
+          {navItems.map((item, idx) => (
+            <ul key={idx}>
+              <li className="hover:text-[#04aeee]">{item}</li>
+            </ul>
+          ))}
+        </div>
+      </div>
+      <div className="flex">
+        <Image
+          src="/NotificationIcon.png"
+          alt="Bell Icon"
+          width={52}
+          height={52}
+        />
+        <button className="btn-primary border-l-2 pl-2">Sign In</button>
+      </div>
+
+      {/* Responsive Navbar Section , might move to a new component */}
+      {navbar && (
+        <NavbarResponsive navItems={navItems} toggleNavbar={toggleNavbar} />
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
