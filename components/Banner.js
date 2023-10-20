68f8fff20b39edf2b1181c83d699f298bb2fcfdb
NullPointError07 <md.taukir.alam@g.bracu.ac.bd>
2023-10-20 10:48:52 +0600

    changed some dependencies

--- components/Banner.js
+++ components/Banner.js
@@ -1,38 +1,4 @@
-"use client";
+'use client'
 
-import carousalItems from "@/utils/carousalItems";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-const Banner = () => {
-  return (
-    <Carousel
-      showArrows={false}
-      showStatus={false}
-      emulateTouch={true}
-      useKeyboardArrows={true}
-      showThumbs={false}
-    >
-      {carousalItems.map((item, index) => (
-        <div
-          key={index}
-          className="image-container relative bg-cover"
-          style={{
-            backgroundImage: `url(${item.image})`,
-            // backgroundSize: "100vw auto",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            height: 800,
-          }}
-        >
-          <div className="content">
-            <h1 className="theme-1 text-2xl">{item.header}</h1>
-            <p>{item.text}</p>
-          </div>
-        </div>
-      ))}
-    </Carousel>
-  );
-};
-
-export default Banner;
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
--- components/VideoCard.js
+++ components/VideoCard.js
@@ -1,51 +1,75 @@
-"use client";
-
 import { useSession } from "next-auth/react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { BsPlayCircle } from 'react-icons/bs';
+import VideoPlayModal from "./VideoPlayModal";
+
 
 const VideoCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [showVideoModal, setShowVideoModal] = useState(false);
+
   const { data: session } = useSession();
   const pathName = usePathname();
-  const router = useRouter();
+  
+  const toggleVideoModal = () => {
+    setShowVideoModal(!showVideoModal);
+  };
 
   return (
-    <div className="border-2 rounded-xl shadow p-4 hover:shadow-2xl cursor:pointer">
-      <div>
+    <div className="border-2 rounded-xl shadow p-4 hover:shadow-2xl cursor-pointer">
+      <div className="relative">
         <video
           className="rounded-xl"
           src={post?.file ? `${post.file.location}` : "cat-video.mp4"}
         />
-        <h1 className="text-lg font-medium">
-          <span className="text-xs">Uploaded by:</span> {post?.creator?.name}
-        </h1>
-        <h1 className="text-lg font-bold">{post?.title}</h1>
-        <p className="text-sm font-medium my-4 text-gray-700">
-          {post?.description}
-        </p>
-        <p
-          className="font-extrabold text-[#04aeee] cursor-pointer"
-          onClick={() => handleTagClick && handleTagClick(post?.tag)}
-        >
-          {post?.tag}
-        </p>
-
-        {session?.user.id === post?.creator?._id && pathName === "/profile" && (
-          <div className="flex justify-center items-stretch pt-4 space-x-3">
-            <p
-              className="text-sm bg-green-500 p-1 rounded-lg text-white cursor-pointer"
-              onClick={handleEdit}
-            >
-              Edit
-            </p>
-            <p
-              className="text-sm bg-red-500 p-1 rounded-lg text-white cursor-pointer"
-              onClick={handleDelete}
-            >
-              Delete
-            </p>
-          </div>
-        )}
+        <div className="absolute inset-0 flex justify-center items-center">
+          <BsPlayCircle
+            onClick={() => {
+              toggleVideoModal();
+            }}
+            className="text-white cursor-pointer"
+            size={40}
+          />
+        </div>
       </div>
+      <h1 className="text-lg font-medium">
+        <span className="text-xs">Uploaded by:</span> {post?.creator?.name}
+      </h1>
+      <h1 className="text-lg font-bold">{post?.title}</h1>
+      <p className="text-sm font-medium my-4 text-gray-700">
+        {post?.description}
+      </p>
+      <p
+        className="font-extrabold text-[#04aeee] cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post?.tag)}
+      >
+        {post?.tag}
+      </p>
+
+      {session?.user.id === post?.creator?._id && pathName === "/profile" && (
+        <div className="flex justify-center items-stretch pt-4 space-x-3">
+          <p
+            className="text-sm bg-green-500 p-1 rounded-lg text-white cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="text-sm bg-red-500 p-1 rounded-lg text-white cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
+
+      {/* Render the VideoModal when showVideoModal is true. */}
+      {showVideoModal && (
+        <VideoPlayModal
+          videoSrc={post?.file ? `${post.file.location}` : "cat-video.mp4"}
+          onClose={toggleVideoModal}
+        />
+      )}
     </div>
   );
 };
